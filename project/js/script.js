0b0b42aa299d51a4d2f011f6f5b8264c56285a70
Lotpite <exstornado@gmail.com>
2020-12-09 20:27:38 +0200

    updated project with alternative solution

--- project/js/script.js
+++ project/js/script.js
@@ -1,3 +1,5 @@
+
+// Метод из урока
 'use strict';
 
 const movieDB = {
@@ -9,32 +11,60 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
-// Сортировка фильмов 
-movieDB.movies.sort();
 
-const some = document.querySelector('.promo__adv'), // работает
-      advImgs = some.getElementsByTagName('img'), // находит рисунки в some как обьект HTML Collection
-      imgs = some.querySelectorAll('img'), // находит рисунки в виде массива Nodelist
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
       genre = document.querySelector('.promo__genre'),
-      bgImg = document.querySelector('.promo__bg'),
-      moviesList = document.querySelector('.promo__interactive-list'),
-      moviesListLi = moviesList.getElementsByTagName('li');
+      moveiList = document.querySelector('.promo__interactive-list');
+console.log(adv);
 
-// Присвоить новый динамический список и упорядочить по номерам
-   for (let i = 0; i < moviesListLi.length; i++) {
-       moviesListLi[i].childNodes[0].textContent = `${i+1}. ${movieDB.movies[i]}`;
-   }
+adv.forEach(item => {
+    item.remove();
+});
 
-// Удалить рекламу от спонсоров (только рисунки)
-    imgs.forEach(item => {
-         item.remove();
-     }); 
-
-/* Удалить рекламный блок promo__adv
-some.remove(); */
-
-// Изменить текст жанра
 genre.textContent = 'драма';
 
-// Изменить рисунок
-bgImg.style = 'background: url(../img/bgImg.jpg) center (center / cover) no-repeat';
+poster.style.backgroundImage =  'url("img/bg.jpg")';
+
+moveiList.innerHTML = "";
+
+movieDB.movies.sort();
+
+
+movieDB.movies.forEach((film, i) => {
+    moveiList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+             <div class="delete"></div>
+        </li>
+    `;
+});
+
+// // Сортировка фильмов 
+// movieDB.movies.sort();
+
+// const some = document.querySelector('.promo__adv'), // работает
+//       advImgs = some.getElementsByTagName('img'), // находит рисунки в some как обьект HTML Collection
+//       imgs = some.querySelectorAll('img'), // находит рисунки в виде массива Nodelist
+//       genre = document.querySelector('.promo__genre'),
+//       bgImg = document.querySelector('.promo__bg'),
+//       moviesList = document.querySelector('.promo__interactive-list'),
+//       moviesListLi = moviesList.getElementsByTagName('li');
+
+// // Присвоить новый динамический список и упорядочить по номерам
+//    for (let i = 0; i < moviesListLi.length; i++) {
+//        moviesListLi[i].childNodes[0].textContent = `${i+1}. ${movieDB.movies[i]}`;
+//    }
+
+// // Удалить рекламу от спонсоров (только рисунки)
+//     imgs.forEach(item => {
+//          item.remove();
+//      }); 
+
+// /* Удалить рекламный блок promo__adv
+// some.remove(); */
+
+// // Изменить текст жанра
+// genre.textContent = 'драма';
+
+// // Изменить рисунок
+// bgImg.style = 'background: url(../img/bgImg.jpg) center (center / cover) no-repeat';
