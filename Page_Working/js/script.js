641e058993d4c9303bd62fd1287c3386aad617f4
Lotpite <exstornado@gmail.com>
2020-12-09 12:03:20 +0200

    Start new project

--- Page_Working/js/script.js
+++ Page_Working/js/script.js
@@ -1,3 +1,63 @@
+//Lesson 2 - 
+"use strict";
+
+const  box = document.getElementById('box'),
+       btns = document.getElementsByTagName('button'),
+       circles = document.getElementsByClassName('circle'),
+       wrapper = document.querySelector('.wrapper'),
+       hearts = wrapper.querySelectorAll('.heart'),
+       oneHeart = wrapper.querySelector('.heart');
+       
+
+box.style.backgroundColor = 'blue';
+box.style.width = '330px';
+const num = '500px';
+
+box.style.cssText = `background-color: pink; width: ${num}`;
+
+btns[1].style.borderRadius = '50%';
+circles[0].style.backgroundColor = 'green';
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'green';
+});
+
+const div = document.createElement('div');
+//const text = document.createTextNode('Test');
+
+div.classList.add('black');
+
+//document.body.append(div);
+ wrapper.append(div); // вставить в конец
+//div.innerHTML = "Hello world";
+div.innerHTML = "<h1>Hello world</h1>"; // можно создавать новую структуру
+//div.textContent = "Hi"; // работает только с текстом
+
+// div.insertAdjacentHTML('beforeend', '<h2>Dobrij den</h2>');
+
+// wrapper.prepend(div); // вставить в начало
+//hearts[0].after(div);
+
+//circles[1].remove();
+
+//hearts[2].replaceWith(circles[2]);
+
+//OLD constructions
+//wrapper.appendChild(div);
+//wrapper.insertBefore(div, hearts[1]);
+//wrapper.removeChild(hearts[1]);
+//wrapper.replaceChild(circles[0], hearts[0]);
+
+
+
+
+
+/*Lesson 1 - How to get Element 
+
 'use strict';
 
 const box = document.getElementById('box');
@@ -20,3 +80,4 @@ hearts.forEach(item => {
 
 const oneHeart = document.querySelector('.heart');
 console.log(oneHeart);
+*/
