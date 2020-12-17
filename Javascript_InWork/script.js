0b34f690b976a472f6882d2850b251108537e431
Lotpite <exstornado@gmail.com>
2020-12-17 16:02:04 +0200

    added classes lesson

--- Javascript_InWork/script.js
+++ Javascript_InWork/script.js
@@ -1,4 +1,162 @@
-// const deadLine = '2020.12.15',
+//Class
+
+"use strict";
+
+// Абстракция отделения шаблона(концепции) от экземпляра, созданого на основе концепции
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+// const square = new Rectangle(10, 10),
+//     long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
+
+// Наследование (иерархия)
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor (height, width, text, bgColor) {
+        super(height, width); // всегда идет первой строчкой
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hi world', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+
+/* This . Call Context
+
+"use strict";
+
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this)
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+
+// showThis(4, 5);
+
+// const obj = {
+//     a : 20,
+//     b : 15,
+//     sum: function() {
+//         function shout() {
+//         console.log(this);
+//     }
+//     shout();
+// }
+// };
+// obj.sum();
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log(`hi ${this.name}`);
+//     };
+// }
+
+// let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this * num; // creates new Function
+
+// }
+
+// const double = count.bind(2);
+// console.log(double(4));
+
+//1) Обычная функция: this = window, но если стоит "use strict" - undefined
+//2) Контекст у методов обьекта - сам обьект
+//3) this в конструкторах и классах - это новый экземпляр обьекта
+//4) Ручная привязка this: call, apply, bind
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() { //работает если нестрелочная функция
+    console.log(this);
+    this.style.backgroundColor = 'red';
+});
+
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this); // возвращает обьект obj
+        };
+
+        say();
+    }
+};
+
+obj.sayNumber();
+
+const double = (a) => a * 2;
+console.log(double(5));
+*/
+
+/* Constructors
+"use strict";
+
+// const num = new Number(3); // старье
+// console.log(num);
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function () {
+        console.log(`hi ${this.name}`);
+    };
+}
+
+User.prototype.exit = function() {
+    console.log(`User ${this.name} gone`);
+};
+
+const andrew = new User('Andrew', 1),
+    alex = new User('Alex', 2);
+
+alex.exit();
+
+andrew.hello();
+alex.hello();
+*/
+
+/*  Dates
+const deadLine = '2020.12.15',
 //       now = new Date();
 
 
@@ -11,7 +169,7 @@
 // let minutes = Math.floor((timeLeft /  1000) % 60);
 // console.log(days);
 // console.log(hours);
-// console.log(minutes);
+/* console.log(minutes);
 
 
 
